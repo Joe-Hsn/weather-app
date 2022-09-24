128c636a81f6176fdb56366b59d351db61fa10ea
Youssef Hassan <youssef.hassan.mahmoud@gmail.com>
2022-09-23 23:06:48 -0700

    tailwindcss added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Search from "./components/search/search";
+import CurrentState from "./components/current-state/current-state";
 export default function App() {
   const handleSearchChange = (searchValue) => {
     console.log(searchValue);
   };
   return (
-    <div className="Container">
-      <Search onSearchChange={handleSearchChange} />
+    <div className="container min-h-screen w-screen mx-auto p-6">
+      <Search onSearchChange={handleSearchChange} className="mb-8" />
+      <CurrentState />
     </div>
   );
 }
